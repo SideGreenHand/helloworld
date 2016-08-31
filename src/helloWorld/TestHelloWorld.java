8dbf6d1cfb707a5428117773490f41e235712e0e
e <jiaoW526@163.com>
2016-08-31 11:23:30 +0800

    Initial commit

---
+++ src/helloWorld/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+
+/**
+ * Created by jiaoW on 2016/8/30.
+ */
+public class TestHelloWorld {
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+}
